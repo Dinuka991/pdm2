9dd581ebe43ebcf029d27899c56c0f3b04d9c145
Darshana Ekanayake <Darshana Ekanayake>
2017-04-11 08:43:30 +0530

    login and signup

---
+++ src/pages/pda/pda.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-PDA',
+  templateUrl: 'pda.html'
+})
+export class PDA {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
